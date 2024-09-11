8742a8b66641773bec7ff05c3672e014410c1513
anu1964 <anukn2006@gmail.com>
2024-09-11 06:38:54 +0530

    Update greatest_3.java

--- greatest_3.java
+++ greatest_3.java
@@ -1,11 +1,23 @@
+import java.util.*;
 class greatest_3
 {
  public static void main(String args[])
  {
-   int a=2,b=1,c=3;
-   a=big;
-   if(b>big) b=big;
-   if(c>big) c=big;
+   Scanner in = new Scanner(System.in);
+   System.out.println("Enter any 3 numbers:");
+   int a= in.nextInt();
+   int b= in.nextInt();
+   int c=in.nextInt();
+   if(a==b||b==c||c==a)
+   {
+       System.out.println("Enter valid numbers");
+   }
+   else
+   {
+   int big=a;
+   if(b>big) big=b;
+   if(c>big) big=c;
    System.out.println("Greatest of"+a+"&"+b+"&"+c+"is"+big);
-  }
+   }
+ }
 }
