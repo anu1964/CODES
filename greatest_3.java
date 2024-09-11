32e0ae85bcc99912e73bf30d9f5f4bc6e5b75d61
anu1964 <anukn2006@gmail.com>
2024-09-11 06:24:47 +0530

    Update

---
+++ greatest_3.java
@@ -0,0 +1,11 @@
+class greatest_3
+{
+ public static void main(String args[])
+ {
+   int a=2,b=1,c=3;
+   a=big;
+   if(b>big) b=big;
+   if(c>big) c=big;
+   System.out.println("Greatest of"+a+"&"+b+"&"+c+"is"+big);
+  }
+}
